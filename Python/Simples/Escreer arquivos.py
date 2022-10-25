7ce0cbd6e4004e5cc154fa63f4de19f420d976fe
Gabriel Torezani <gabrielctorezani@gmail.com>
2022-10-24 21:48:33 -0300

    New

---
+++ Python/Simples/Escreer arquivos.py
@@ -0,0 +1,2 @@
+with open('Texto.txt', 'w') as arquivo:
+    arquivo.write('Teste de python escrevendo')
---
+++ Python/Simples/ModosArquivo.py
@@ -0,0 +1,7 @@
+""""
+r > Abre para leitura - padrão
+w > Abre para escrita - sobreescreve caso exista e cria caso não exista
+x > Abre para escrita caso não existir
+a > Abre para escrita e adiciona a escrita ao final do arquivo
++ > Abre e substitui
+"""
